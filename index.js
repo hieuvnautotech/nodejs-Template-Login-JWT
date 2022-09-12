25833773091e1afa22f8aa6cd3ad6d2d37a4f214
hieu <hieuvnautotech@gmail.com>
2022-09-12 08:50:52 +0700

    cty

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const path = require("path");
 const cors = require("cors");
 const bodParser = require("body-parser");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 
 dotenv.config();
 
+// connect db kiểu biến môi trường
+mongoose.connect(
+    process.env.DB_CONNECT,
+    {useNewUrlParser: true},
+    () => console.log('connected to db!')
+);
 
 const app = express();
+app.use(express.json());//middleware
 
 app.use(expressLayouts);
 app.set("view engine", "ejs");
